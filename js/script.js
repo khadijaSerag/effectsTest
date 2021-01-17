f376a4c7b8df7510dba90db5964950b8380a600b
khadijaSerag <khadija.serag.98@gmail.com>
2021-01-17 03:55:49 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
+
+
+$("#ddemo").animate({ width: '100%' }, 1500);
+$("#ddemo").animate({ height: '250%' }, 1500, function () {
+    $("#sec2").show(1000, function () {
+        $(".welcome h1").show(1000, function () {
+            $("#cont").show(1000, function () {
+                $("#secc1").fadeIn(1000, function () {
+                    $("#demo").hide(1500);
+                    $("#demo").show(1500, function () {
+                        $("#secc2").show(1000, function () {
+                            $("#demoo1").slideUp(1500);
+                            $("#demoo1").slideDown(1500, function () {
+                                $("#secc3").slideDown(1000, function () {
+                                    $("#demo4").fadeOut(1500);
+                                    $("#demo4").fadeIn(1500);
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+
+$("#start").click(function () {
+    $("#wide").animate({ width: '100%' }, 2000);
+    $("#wide").animate({ height: '50vh' }, 2000);
+    $("#wide").animate({ borderRadius: '50%' }, 2000);
+})
+$("#stop").click(function () {
+    $("#wide").stop();
+});
 // ...................... hide & show & toggle..................
-$("#demo").hide(3000);
-$("#demo").show(2000);
+// $("#demo").hide(3000);
+// $("#demo").show(2000);
 // $(".btn").click();
 $("#mar").hide();
 
@@ -17,8 +51,8 @@ $("#btn2").click(function () {
 })
 
 // ...................... slideUp & slideDown & slideToggle..................
-$("#demoo1").slideUp(3000);
-$("#demoo1").slideDown(2500);
+// $("#demoo1").slideUp(3000);
+// $("#demoo1").slideDown(2500);
 $("#yes").hide();
 
 $("#btnn1").click(function () {
@@ -33,16 +67,16 @@ $("#btnn2").click(function () {
 })
 
 // ...................... fadeIn & fadeOut & fadeToggle & fadeTo ..................
-$("#demo4").fadeOut(5000);
-$("#demo4").fadeIn(5000);
+// $("#demo4").fadeOut(5000);
+// $("#demo4").fadeIn(5000);
 
-$("#btn5").click(function(){
+$("#btn5").click(function () {
     $("#demo5").fadeOut(1000);
     $("#demo5").fadeIn(1000);
 });
-$("#btn6").click(function(){
+$("#btn6").click(function () {
     $("#demo6").fadeToggle(2000);
 });
-$("#btn7").click(function(){
-    $("#demo7").fadeTo(2000,0.8);
+$("#btn7").click(function () {
+    $("#demo7").fadeTo(2000, 0.8);
 });
