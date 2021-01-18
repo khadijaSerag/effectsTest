010355234d0e8d413ec56620a460093c38504bca
khadijaSerag <khadija.serag.98@gmail.com>
2021-01-18 13:10:56 +0200

    update3

--- js/script.js
+++ js/script.js
@@ -4,20 +4,40 @@ $("#ddemo").animate({ width: '100%' }, 1500);
 $("#ddemo").animate({ height: '250%' }, 1500, function () {
     $(".welcome").fadeIn(1000, function () {
         $("#sec2").show(2000, function () {
-            $("#cont").show(1000, function () {
-                $("#secc1").fadeIn(1500, function () {
-                    $("#demo").hide(1500);
-                    $("#demo").show(1500, function () {
-                        $("#secc2").show(1500, function () {
-                            $("#demoo1").slideUp(1500);
-                            $("#demoo1").slideDown(1500, function () {
-                                $("#secc3").slideDown(1500, function () {
-                                    $("#demo4").fadeOut(1500);
-                                    $("#demo4").fadeIn(1500);
+            // $("#cont").show(1000, function () {
+            $("#secc1").fadeIn(1500, function () {
+                $(".cont1").show(1000, function () {
+                    $(".cont2").show(1000, function () {
+                        $(".cont3").show(1000, function () {
+                            $("#demo").hide(1500);
+                            $("#demo").show(1500, function () {
+                                $("#secc2").show(1500, function () {
+                                    $(".cont4").show(1000, function () {
+                                        $(".cont5").show(1000, function () {
+                                            $(".cont6").show(1000, function () {
+                                                $("#demoo1").slideUp(1500);
+                                                $("#demoo1").slideDown(1500, function () {
+                                                    $("#secc3").slideDown(1500, function () {
+                                                        $(".cont7").show(1000, function () {
+                                                            $(".cont8").show(1000, function () {
+                                                                $(".cont9").show(1000, function () {
+                                                                    $(".cont10").show(1000, function () {
+                                                                        $("#demo4").fadeOut(1500);
+                                                                        $("#demo4").fadeIn(1500);
+                                                                    });
+                                                                });
+                                                            });
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    });
                                 });
                             });
                         });
                     });
+                    // });
                 });
             });
         });
@@ -31,7 +51,7 @@ $("#start").click(function () {
     $("#wide").animate({ borderRadius: '50%' }, 2000);
 })
 $("#stop").click(function () {
-    $("#wide").stop();
+    $("#wide").stop(true, false);
 });
 // ...................... hide & show & toggle..................
 // $("#demo").hide(3000);
@@ -44,7 +64,10 @@ $("#btn1").click(function () {
     $("#demo2").hide(1000);
     $("#demo2").show(2000);
     // document.getElementById("mar").classList = "d-block";
-    $("#mar").show(2000);
+    // $("#mar").show(2000);
+})
+$("#bbtn1").click(function () {
+    $("#mar").toggle(1000);
 })
 $("#btn2").click(function () {
     // window.alert("hello");
